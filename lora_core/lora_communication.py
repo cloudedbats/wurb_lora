66d3334b9dcae1286ae0cd128b6d10efb7d1a244
Arnold Andreasson <arnold@mellifica.se>
2025-04-06 19:04:14 +0200

    LoRa code updated.

--- lora_core/lora_communication.py
+++ lora_core/lora_communication.py
@@ -27,100 +27,112 @@ class LoraCommunication(object):
 
     def clear(self):
         """ """
-        self.lora = None
-        self.send_worker = None
-        self.receive_worker = None
-        self.parser_worker = None
+        self.serial = None
+        self.send_task = None
+        self.receive_task = None
+        self.parser_task = None
         self.lora_out_queue = None
         self.lora_in_queue = None
 
     def configure(self):
         """ """
-        self.queue_max_size = 10000
+        self.queue_max_size = 1000
         self.selected_idx_part = "1A86:55D3"  # "USB Single Serial" for USB-TO-LoRa-LF.
+
+    def is_lora_connected(self):
+        """ """
+        if self.serial != None:
+            try:
+                self.serial.inWaiting()
+                return self.serial.isOpen()
+            except:
+                return False
+        else:
+            return False
 
     def lora_send(self, data):
         """ """
+        if not self.is_lora_connected():
+            return
         try:
             if self.lora_out_queue != None:
                 # print("SEND: ", data)
                 self.lora_out_queue.put_nowait(data)
         except Exception as e:
             print("A: ", e)
-            self.logger.debug("LoraWorker, lora_send " + str(e))
-
-    def select_comport(self):
-        """ """
-        result = None
-        try:
-            print("List of comports:")
-            for info in serial.tools.list_ports.comports(
-                # include_links=True,
-            ):
-                if info.hwid in ["n/a", None, ""]:
-                    continue
-                print("device: ", info.device)
-                # print("name: ", info.name)
-                print("hwid: ", info.hwid)
-                # print("vid: ", info.vid)
-                # print("pid: ", info.pid)
-                # print("serial_number: ", info.serial_number)
-                # print("location: ", info.location)
-                # print("manufacturer: ", info.manufacturer)
-                print("product: ", info.product)
-                # print("interface: ", info.interface)
-
-                if self.selected_idx_part in info.hwid:
-                    result = info.device
-                print("")
-        except Exception as e:
-            print("C: ", e)
-            self.logger.debug("LoraWorker, select_comport " + str(e))
-        return result
-
-    def lora_connect(self, comport):
-        """ """
-        try:
-            self.lora_disconnect()
-            #
-            self.lora = serial.Serial(
-                comport,
-                baudrate=115200,
-                parity=serial.PARITY_NONE,
-                stopbits=serial.STOPBITS_ONE,
-                bytesize=serial.EIGHTBITS,
-                timeout=2,
-            )
-        except Exception as e:
-            print("D: ", e)
-            self.logger.debug("LoraWorker, lora_connect" + str(e))
-
-    def lora_disconnect(self):
-        """ """
-        try:
-            if self.lora != None:
-                if self.lora.isOpen():
-                    self.lora.close()
-        except Exception as e:
-            print("E: ", e)
-            self.logger.debug("LoraWorker, lora_disconnect" + str(e))
+            self.logger.debug("LoraComm, lora_send " + str(e))
 
     async def lora_at_config(self, at_commands):
         """ """
-        # await asyncio.sleep(3)
-        self.lora.write("+++\r\n".encode())
-        await asyncio.sleep(0.1)
+        if not self.is_lora_connected():
+            return
+        await asyncio.sleep(5)
+        self.serial.write("+++\r\n".encode())
+        await asyncio.sleep(0.2)
         for command in at_commands:
             data = command + "\r\n"
-            self.lora.write(data.encode())
-        await asyncio.sleep(1)
+            self.serial.write(data.encode())
+        await asyncio.sleep(5)
+
+    def check_devices(self, device_hwid_part):
+        """ """
+        selected_device = None
+        try:
+            self.logger.debug("LoRA: Connected devices: ")
+            for info in serial.tools.list_ports.comports():
+                if info.hwid in ["n/a", None, ""]:
+                    continue
+                self.logger.debug("LoRa: hwid: " + info.hwid)
+                if device_hwid_part in info.hwid:
+                    selected_device = info.device
+        except Exception as e:
+            print("C: ", e)
+            self.logger.debug("LoraComm, select_comport " + str(e))
+        return selected_device
+
+    def open_serial(
+        self,
+        selected_device,
+        baudrate=115200,
+        timeout=1,
+    ):
+        """ """
+        self.serial = None
+        try:
+            if selected_device != None:
+                self.serial = serial.Serial(
+                    selected_device,
+                    baudrate=baudrate,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=timeout,
+                )
+                self.serial.reset_input_buffer()
+                self.serial.reset_output_buffer()
+        except Exception as e:
+            print("D: ", e)
+            self.logger.debug("LoraComm, open_serial: " + str(e))
+
+    def close_serial(self):
+        """ """
+        try:
+            if self.serial != None:
+                if self.serial.isOpen():
+                    self.serial.close()
+                self.serial = None
+        except Exception as e:
+            print("E: ", e)
+            self.logger.debug("LoraComm, close_serial: " + str(e))
 
     async def startup(self):
         """ """
         try:
-            selected_device = self.select_comport()
-            if selected_device != None:
-                self.lora_connect(selected_device)
+            selected_device = self.check_devices(self.selected_idx_part)
+            if selected_device == None:
+                return False
+            else:
+                self.open_serial(selected_device)
                 # Create queues.
                 if self.lora_out_queue == None:
                     self.lora_out_queue = asyncio.Queue(maxsize=self.queue_max_size)
@@ -129,35 +141,42 @@ class LoraCommunication(object):
                 # Clear queues if created earlier.
                 self.remove_items_from_queue(self.lora_out_queue)
                 self.remove_items_from_queue(self.lora_in_queue)
-                # Start send worker.
-                if self.send_worker == None:
-                    self.send_worker = asyncio.create_task(
-                        self.lora_send_worker(), name="LoRa-send-task"
+                # Start send task.
+                if self.send_task == None:
+                    self.send_task = asyncio.create_task(
+                        self.lora_send_task(), name="LoRa-send-task"
                     )
                     self.logger.debug("LoRa-send-task STARTED.")
-                # Start receive worker.
-                if self.receive_worker == None:
-                    self.receive_worker = asyncio.create_task(
-                        self.lora_receive_worker(), name="LoRa-receive-task"
+                # Start receive task.
+                if self.receive_task == None:
+                    self.receive_task = asyncio.create_task(
+                        self.lora_receive_task(), name="LoRa-receive-task"
                     )
                     self.logger.debug("LoRa-receive-task STARTED.")
-                # Start parser worker.
-                if self.parser_worker == None:
-                    self.parser_worker = asyncio.create_task(
-                        self.lora_parser_worker(), name="LoRa-parser-task"
+                # Start parser task.
+                if self.parser_task == None:
+                    self.parser_task = asyncio.create_task(
+                        self.lora_parser_task(), name="LoRa-parser-task"
                     )
                     self.logger.debug("LoRa-parser-task STARTED.")
                 #
                 await asyncio.sleep(0)
+            return True
         except Exception as e:
             print("F: ", e)
-            self.logger.debug("LoraWorker, start_lora " + str(e))
+            self.logger.debug("LoraComm, start_lora " + str(e))
+            return False
 
-    async def lora_send_worker(self):
+    async def shutdown(self):
+        """ """
+
+    async def lora_send_task(self):
         """ """
         try:
             while True:
                 try:
+                    if not self.is_lora_connected():
+                        break
                     item = await self.lora_out_queue.get()
                     print("SEND-Q: ", item)
                     try:
@@ -170,7 +189,7 @@ class LoraCommunication(object):
                             # Send LoRa message.
                             # print("LoRa write: ", item)
                             data = item + "\r\n"
-                            self.lora.write(data.encode())
+                            self.serial.write(data.encode())
                             # print("LoRa write done: ", item)
                     finally:
                         self.lora_out_queue.task_done()
@@ -181,27 +200,29 @@ class LoraCommunication(object):
                     break
                 except Exception as e:
                     print("G: ", e)
-                    message = "RecWorker - rec_send_worker(1): " + str(e)
+                    message = "LoraComm,  LoRa send(1): " + str(e)
                     self.logger.debug(message)
-
                 await asyncio.sleep(0)
-
         except Exception as e:
             print("H: ", e)
-            message = "RecWorker - rec_send_worker(2). Exception: " + str(e)
+            message = "LoraComm,  LoRa send(2). Exception: " + str(e)
             self.logger.debug(message)
         finally:
-            self.logger.debug("LoRaWorker - LoRa send ended.")
+            self.close_serial()
+            self.send_task = None
+            self.logger.debug("LoraComm - LoRa send ended.")
 
-    async def lora_receive_worker(self):
+    async def lora_receive_task(self):
         """ """
         buffer = b""
         try:
             try:
                 while True:
                     try:
-                        if self.lora.inWaiting() > 0:
-                            buffer += self.lora.read(self.lora.inWaiting())
+                        if not self.is_lora_connected():
+                            break
+                        if self.serial.inWaiting() > 0:
+                            buffer += self.serial.read(self.serial.inWaiting())
                             # print("BUFFER: ", buffer.replace(b"\r\n", b"--", -1).decode())
                             while b"\r\n" in buffer:
                                 index = buffer.index(b"\r\n")
@@ -212,22 +233,28 @@ class LoraCommunication(object):
                                     # print(data.decode())
                                     self.lora_in_queue.put_nowait(data.decode())
                                 except Exception as e:
-                                    print("Exception: ", e)
+                                    print(
+                                        "LoraComm, LoRa receive(1). Exception: ", str(e)
+                                    )
                         await asyncio.sleep(0.1)
                     except serial.SerialException:
-                        print("SerialException.")
+                        self.serial = None
+                        print("LoraComm, SerialException.")
                     except Exception as e:
-                        print("I: ", e)
-                        self.logger.debug("LoraWorker, ???: " + str(e))
+                        self.serial = None
+                        self.logger.debug(
+                            "LoraComm, LoRa receive(2). Exception: " + str(e)
+                        )
                     await asyncio.sleep(0.1)
             finally:
-                self.lora.close()
+                self.close_serial()
+                self.receive_task = None
                 print("Closed.")
         except Exception as e:
             print("J: ", e)
-            self.logger.debug("LoraWorker, ???: " + str(e))
+            self.logger.debug("LoraComm, LoRa receive(3). Exception:  " + str(e))
 
-    async def lora_parser_worker(self):
+    async def lora_parser_task(self):
         """ """
         try:
             while True:
@@ -245,6 +272,7 @@ class LoraCommunication(object):
                             if "TEST" in item:
                                 data = item.replace("TEST", "ACK")
                                 self.lora_send(data)
+
                     finally:
                         self.lora_in_queue.task_done()
                         await asyncio.sleep(0)
@@ -254,17 +282,18 @@ class LoraCommunication(object):
                     break
                 except Exception as e:
                     print("K: ", e)
-                    message = "RecWorker - rec_parser_worker(1): " + str(e)
+                    message = "LoraComm - LoRa parser(1): " + str(e)
                     self.logger.debug(message)
 
                 await asyncio.sleep(0)
 
         except Exception as e:
             print("L: ", e)
-            message = "RecWorker - rec_parser_worker(2). Exception: " + str(e)
+            message = "LoraComm - LoRa parser(2). Exception: " + str(e)
             self.logger.debug(message)
         finally:
-            self.logger.debug("LoRaWorker - LoRa parser ended.")
+            self.parser_task = None
+            self.logger.debug("LoraComm - LoRa parser ended.")
 
     def remove_items_from_queue(self, queue):
         """Helper method."""
--- lora_core/lora_manager.py
+++ lora_core/lora_manager.py
@@ -4,6 +4,7 @@
 # Author: Arnold Andreasson, info@cloudedbats.org
 # License: MIT http://opensource.org/licenses/mit
 
+import asyncio
 import logging
 import lora_core
 import lora_core.file_checker
@@ -28,7 +29,7 @@ class LoraManager:
         """ """
         self.is_lora_active = False
         self.is_file_checker_active = False
-        self.check_lora_worker = None
+        self.lora_task = None
 
     def configure(self):
         """ """
@@ -44,13 +45,60 @@ class LoraManager:
 
     async def startup(self):
         """ """
-        await lora_core.lora_comm.startup()
-        await lora_core.lora_comm.lora_at_config(self.at_commands)
-        if self.is_file_checker_active == False:
-            await lora_core.file_checker.startup()
-            self.is_file_checker_active = True
+        #
+        try:
+            if self.is_file_checker_active == False:
+                await lora_core.file_checker.startup()
+                self.is_file_checker_active = True
+        except Exception as e:
+            print("LoraManager, startup(1). Exception: ", str(e))
+        #
+        try:
+            if self.lora_task == None:
+                self.lora_task = asyncio.create_task(
+                    self.lora_check_task(), name="LoRa-activate-task"
+                )
+                self.logger.debug("LoRa-send-task STARTED.")
+        except Exception as e:
+            print("F: ", e)
+            self.logger.debug("LoraManager, startup(2). Exception: " + str(e))
 
     async def shutdown(self):
         """ """
+        if self.lora_task != None:
+            self.lora_task.cancel()
         await lora_core.lora_comm.shutdown()
         await lora_core.file_checker.shutdown()
+
+    async def lora_check_task(self):
+        """ """
+        lora_connected_old = False
+        try:
+            while True:
+                try:
+                    if not lora_core.lora_comm.is_lora_connected():
+                        is_device_available = await lora_core.lora_comm.startup()
+                        if is_device_available:
+                            if lora_core.lora_comm.is_lora_connected():
+                                self.logger.debug(
+                                    "LoraManager: LoRa execute AT commands."
+                                )
+                                await lora_core.lora_comm.lora_at_config(
+                                    self.at_commands
+                                )
+                    #
+                    lora_connected = lora_core.lora_comm.is_lora_connected()
+                    if lora_connected != lora_connected_old:
+                        lora_connected_old = lora_connected
+                        if lora_connected:
+                            self.logger.debug("LoraManager: LoRa active.")
+                        else:
+                            self.logger.debug("LoraManager: LoRa NOT active.")
+                except:
+                    pass
+                #
+                await asyncio.sleep(5)
+        except asyncio.CancelledError:
+            self.logger.debug("LoraManager:LoRa task was cancelled.")
+        finally:
+            await lora_core.lora_comm.close_lora()
